cf9b56c45eecdd72a81b3423d714433d9a83b8ca
stg <stefano.giancristofaro.94@gmail.com>
2015-12-02 18:59:55 +0100

    NavigationDrawer edited

--- src/main/java/model/Amministratore.java
+++ src/main/java/model/Amministratore.java
@@ -1,47 +1,22 @@
 package model;
 
 import controller.CatalogHandler;
-import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.collections.FXCollections;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.print.Collation;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.ToolBar;
 import javafx.scene.control.cell.ComboBoxListCell;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.stage.Stage;
 
-import view.CatalogView;
-import view.ConsolePane;
+import view.material.ConsolePane;
 import view.material.NavigationDrawer;
-
-import java.util.List;
 
 public class Amministratore extends Ruolo {
 
     @Override
     public Scene generateView() {
-
-        Label title = new Label();
-        title.setStyle("-fx-text-fill: snow");
-        ToolBar toolbar = new ToolBar(title);
-        toolbar.setStyle("-fx-background-color: cornflowerblue");
-        toolbar.setMinHeight(72);
-
-        ListView<String> list = new ListView<String>(FXCollections.<String>observableArrayList("Visualizza Catalogo",
-                "OPERATION 2",  "OPERATION 3", "Logout"));
-        list.setCellFactory(ComboBoxListCell.forListView(list.getItems()));
-
-        NavigationDrawer drawer = new NavigationDrawer(list);
 
         Label welcome = new Label("Welcome");
         welcome.setPadding(new Insets(25, 25, 25, 25));
@@ -50,9 +25,9 @@ public class Amministratore extends Ruolo {
         welcome.setAlignment(Pos.CENTER);
 
         ConsolePane container = new ConsolePane("Trip Broker Administration");
-        container.setDrawer(drawer);
+        container.setDrawer(FXCollections.<String>observableArrayList("Visualizza Catalogo",
+                "OPERATION 2",  "OPERATION 3", "Logout"));
         container.setCenter(welcome);
-        list.setOnMouseClicked(new CatalogHandler(list, container));
 
         return new Scene(container);
     }
--- src/main/java/view/material/NavigationDrawer.java
+++ src/main/java/view/material/NavigationDrawer.java
@@ -1,10 +1,14 @@
 package view.material;
 
+import controller.CatalogHandler;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Screen;
@@ -13,9 +17,9 @@ public class NavigationDrawer extends VBox {
 
     static double ratio = 0.67;
 
-    ListView options;
+    ListView<String> options;
 
-    public NavigationDrawer(ListView listView) {
+    public NavigationDrawer() {
 
         double width = Screen.getPrimary().getVisualBounds().getWidth() * 1 / 4;
         setPrefWidth(width);
@@ -27,9 +31,27 @@ public class NavigationDrawer extends VBox {
         context.setFill(Color.ORANGE);
         context.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
 
-        options = listView;
+        getChildren().add(canvas);
+    }
+
+    public NavigationDrawer(ObservableList<String> opts, ConsolePane pane) {
+
+        this();
+        setOptions(opts, pane);
+    }
+
+    public void setOptions(ObservableList<String> opts, ConsolePane container) {
+
+        int size;
+        if ((size = getChildren().size()) > 1) getChildren().remove(1, size);
+
+        options = new ListView<>(opts);
+        options.setCellFactory(param -> new DrawerCell());
+        options.minHeight(Double.MAX_VALUE);
         options.setPadding(new Insets(16, 16, 16, 16));
 
-        getChildren().addAll(canvas, options);
+        options.setOnMouseClicked(new CatalogHandler(options, container));
+
+        getChildren().add(options);
     }
 }
