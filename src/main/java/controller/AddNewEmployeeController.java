eb01f331a5649fe2489c4397d335795606d40897
caim03 <christian.lariccia@gmail.com>
2016-01-07 15:17:56 +0100

    AddNewEmployeeController

---
+++ src/main/java/controller/AddNewEmployeeController.java
@@ -0,0 +1,28 @@
+package controller;
+
+
+import model.DBManager;
+import model.dao.DipendentiDaoHibernate;
+import model.daoInterface.DAO;
+import model.entityDB.DipendentiEntity;
+
+public class AddNewEmployeeController {
+
+    public static void handle(String name, String surname, String password, String role, String mail){
+
+        DipendentiEntity entity = new DipendentiEntity();
+
+        entity.setNome(name);
+        entity.setCognome(surname);
+        entity.setPasswordLogin(password);
+        entity.setRuolo(role);
+        entity.setMail(mail);
+
+        DAO dao = DipendentiDaoHibernate.instance();
+        DBManager.initHibernate();
+        dao.store(entity);
+        DBManager.shutdown();
+    }
+
+
+}
