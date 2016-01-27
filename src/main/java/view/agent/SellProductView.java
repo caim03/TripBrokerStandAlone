f2f73bbfc081cf6dfb511f73e0531dd7d714954a
stg <stefano.giancristofaro.94@gmail.com>
2016-01-27 19:05:22 +0100

    SellProductView alpha (TODO DB interaction)

--- src/main/java/view/agent/SellProductView.java
+++ src/main/java/view/agent/SellProductView.java
@@ -2,29 +2,40 @@ package view.agent;
 
 import controller.Constants;
 import controller.SearchProductController;
+import controller.builder.EntityBuilder;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import jfxtras.scene.control.CalendarTimeTextField;
+import model.entityDB.EventoEntity;
+import model.entityDB.OffertaEntity;
+import model.entityDB.PernottamentoEntity;
+import model.entityDB.ViaggioEntity;
 import org.controlsfx.control.Notifications;
 import view.Collector;
 import view.material.*;
 
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Locale;
 
-public class SellProductView extends LayerPane implements Collector {
+public class SellProductView extends LayerPane {
 
-    private VBox vBox;
-    private Node[] offerNode;
+    private OfferOptions options;
     private MaterialSpinner spinner;
     private MaterialButton button;
-    private DBListView listView;
-    private GridPane pane;
+    private ListView listView;
 
     public SellProductView() {
 
@@ -33,194 +44,307 @@ public class SellProductView extends LayerPane implements Collector {
 
         button = new ElevatedButton("Cerca");
 
-        pane = new GridPane();
+        GridPane pane = new GridPane();
         pane.setStyle("-fx-background-color: white");
         pane.setHgap(25);
         pane.setVgap(8);
-        pane.setPadding(new Insets(25, 25, 25, 25));
+        pane.setPadding(new Insets(25));
 
         pane.add(spinner, 0, 0, 5, 1);
         pane.add(button, 6, 0);
 
+        options = new EmptyOptions();
+
+        final VBox vBox = new VBox(pane, options);
+
         button.setOnMouseClicked(event -> {
-            harvest();
-            try {
-            listView.refresh();
-            } catch (NullPointerException nullPointer) {
-                Notifications.create().title("Campi vuoti").text("Riempire tutti i campi obbligatori").show();
+
+            vBox.getChildren().remove(listView);
+
+            String query = options.getQuery();
+            if (query == null) {
+                listView = new ListView();
                 return;
             }
-            vBox.getChildren().clear();
+            listView = new DBListView(query);
+            listView.refresh();
+
             vBox.getChildren().add(listView);
         });
 
         spinner.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue == null) return;
+            vBox.getChildren().remove(options);
+            vBox.getChildren().remove(listView);
 
-            try { vBox.getChildren().remove(1); } catch (IndexOutOfBoundsException ignore) {}
+            if (Constants.travel.equals(newValue)) options = new TravelOptions();
+            else if (Constants.event.equals(newValue)) options = new EventOptions();
+            else if (Constants.stay.equals(newValue)) options = new OvernightOptions();
+            else options = new EmptyOptions();
 
-            vBox.getChildren().add(fromOffer(newValue));
+            vBox.getChildren().add(options);
+            options.generate();
         });
-
-        vBox = new VBox(pane, fromOffer(spinner.getValue()));
 
         attach(vBox);
     }
 
-    private Node fromOffer(String type) {
-        /** @param String; string that represents the type of the offer:
-         *               - Event
-         *               - Travel
-         *               - Stay
-         *               - Packet
-         *  @return Node; return a node to attach at the main pane **/
+    abstract private class OfferOptions extends GridPane {
 
-        Node attachment;
+        protected TextField cityField;
+        protected CheckBox dateCheck;
+        protected DatePicker mainDatePicker;
 
-        if (Constants.travel.equals(type))
-            attachment = travelAttachment();
+        protected OfferOptions() {
+            setStyle("-fx-background-color: white");
+            setHgap(25);
+            setVgap(8);
+            setPadding(new Insets(25));
+        }
 
-        else if (Constants.event.equals(type))
-            attachment = eventAttachment();
+        protected void fields() {
 
-        else if (Constants.stay.equals(type))
-            attachment = stayAttachment();
+            cityField = new MaterialTextField();
+            add(cityField, 1, 0);
+        }
 
-        else if (Constants.packet.equals(type))
-            attachment = packetAttachment();
+        protected void pickers() {
 
-        else attachment = new Pane();
+            mainDatePicker = new DatePicker(LocalDate.now());
+            add(mainDatePicker, 3, 0);
+        }
 
-        return attachment;
+        protected void checkbox() {
+
+            dateCheck = new CheckBox();
+            add(dateCheck, 2, 0);
+
+            dateCheck.selectedProperty().addListener((observable, oldValue, newValue) -> {
+                if (newValue != null && !newValue.equals(oldValue))
+                    mainDatePicker.setDisable(!newValue);
+            });
+            dateCheck.setSelected(false);
+            mainDatePicker.setDisable(true);
+        }
+
+        protected void generate() {
+
+            fields();
+            pickers();
+            checkbox();
+        }
+        abstract String getQuery();
+
+        protected String getCity() { return cityField.getText(); }
+        protected Timestamp getMainDate() {
+
+            LocalDate localDate = mainDatePicker.getValue();
+            if (localDate == null) return null;
+            Instant instant = localDate.atStartOfDay(ZoneId.systemDefault()).toInstant();
+
+            return Timestamp.from(instant);
+        }
     }
 
-    private Node stayAttachment() {
-        /** @return Node; return a node to attach at the main pane **/
+    private class TravelOptions extends OfferOptions {
 
-        offerNode = new Node[3];
+        private TextField destination;
+        private CheckBox arrivalCheck;
+        private DatePicker arrivalDatePicker;
 
-        Label city = new Label("City");
-        TextField ctyField = new MaterialTextField();
-        ctyField.setPromptText("Insert city");
+        @Override
+        protected void fields() {
 
-        Label checkIn = new Label("Check-in");
-        DatePicker datePicker = new DatePicker(LocalDate.now());
+            super.fields();
 
-        Label checkOut = new Label("Check-out");
-        DatePicker endPicker = new DatePicker(LocalDate.now());
+            destination = new MaterialTextField();
 
-        GridPane pane = new GridPane();
-        pane.setStyle("-fx-background-color: white");
-        pane.setHgap(25);
-        pane.setVgap(8);
+            cityField.setPromptText("Inserisci città di partenza");
+            destination.setPromptText("Inserisci destinazione");
 
-        pane.add(city, 0, 1);
-        pane.add(checkIn, 0, 2);
-        pane.add(checkOut, 0, 3);
+            add(new Label("Partenza"), 0, 0);
+            add(new Label("Arrivo"), 0, 1);
+            add(destination, 1, 1);
+        }
 
-        pane.add(ctyField, 1, 1);
-        pane.add(datePicker, 1, 2);
-        pane.add(endPicker, 1, 3);
+        @Override
+        protected void pickers() {
+            super.pickers();
 
-        pane.setPadding(new Insets(25, 25, 25, 25));
+            arrivalDatePicker = new DatePicker(LocalDate.now());
+            add(arrivalDatePicker, 3, 1);
+        }
 
-        offerNode[0] = ctyField;
-        offerNode[1] = datePicker;
-        offerNode[2] = endPicker;
+        @Override
+        protected void checkbox() {
+            super.checkbox();
 
-        return pane;
+            arrivalCheck = new CheckBox();
+            add(arrivalCheck, 2, 1);
+
+            arrivalCheck.selectedProperty().addListener((observable, oldValue, newValue) -> {
+                if (newValue != null && !newValue.equals(oldValue))
+                    arrivalDatePicker.setDisable(!newValue);
+            });
+            arrivalCheck.setSelected(false);
+            arrivalDatePicker.setDisable(true);
+        }
+
+        @Override
+        String getQuery() {
+            String query = "from ViaggioEntity", where = "",
+                   dep = "", arr = "", depTime = "", arrTime = "";
+
+            boolean clause = false;
+            String buf;
+
+            if ((buf = cityField.getText()) != null && !"".equals(buf)) {
+                clause = true;
+                where = " where ";
+                dep += "città like '" + buf + "'";
+            }
+            if ((buf = destination.getText()) != null && !"".equals(buf)) {
+                if (clause) arr += " and ";
+                else {
+                    where = " where ";
+                    clause = true;
+                }
+                arr += "destinazione like '" + buf + "'";
+            }
+            if (dateCheck.isSelected() && mainDatePicker.getValue() != null) {
+
+                if (clause) depTime += " and ";
+                else {
+                    where = " where ";
+                    clause = true;
+                }
+
+                Instant instant = mainDatePicker.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant();
+                Timestamp from = Timestamp.from(instant),
+                        to = Timestamp.from(instant.plus(Duration.ofHours(24)));
+                depTime += "dataInizio between ";
+                depTime += "str_to_date('";
+                depTime += new SimpleDateFormat("yyyy-MM-dd HH:mm").format(from);
+                depTime += "', '%Y-%m-%d %k:%i') and ";
+                depTime += "str_to_date('";
+                depTime += new SimpleDateFormat("yyyy-MM-dd HH:mm").format(to);
+                depTime += "', '%Y-%m-%d %k:%i')";
+            }
+            if (arrivalCheck.isSelected() && arrivalDatePicker.getValue() != null) {
+
+                if (clause) arrTime += " and ";
+                else {
+                    where = " where ";
+                    clause = true;
+                }
+
+                Instant instant = arrivalDatePicker.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant();
+                Timestamp from = Timestamp.from(instant),
+                        to = Timestamp.from(instant.plus(Duration.ofHours(24)));
+                arrTime += "dataInizio between ";
+                arrTime += "str_to_date('";
+                arrTime += new SimpleDateFormat("yyyy-MM-dd HH:mm").format(from);
+                arrTime += "', '%Y-%m-%d %k:%i') and ";
+                arrTime += "str_to_date('";
+                arrTime += new SimpleDateFormat("yyyy-MM-dd HH:mm").format(to);
+                arrTime += "', '%Y-%m-%d %k:%i')";
+            }
+
+            return query + where + dep + arr + depTime + arrTime;
+        }
     }
 
-    private Node eventAttachment() {
-        /** @return Node; return a node to attach at the main pane **/
+    private class EventOptions extends OfferOptions {
 
-        offerNode = new Node[2];
-        Label city = new Label("City");
-        TextField ctyField = new MaterialTextField();
-        ctyField.setPromptText("Insert city");
+        @Override
+        protected void fields() {
 
-        Label date = new Label("Date");
-        DatePicker datePicker = new DatePicker(LocalDate.now());
+            super.fields();
+            cityField.setPromptText("Inserisci città");
+            add(new Label("Città"), 0, 0);
+        }
 
-        GridPane pane = new GridPane();
-        pane.setStyle("-fx-background-color: white");
-        pane.setHgap(25);
-        pane.setVgap(8);
+        @Override
+        String getQuery() {
+            String query = "from EventoEntity", where = "", city = "", date = "";
 
-        pane.add(city, 0, 1);
-        pane.add(date, 0, 2);
+            boolean clause = false;
+            String buf;
 
-        pane.add(ctyField, 1, 1);
-        pane.add(datePicker, 1, 2);
+            if ((buf = cityField.getText()) != null && !"".equals(buf)) {
+                clause = true;
+                where = " where ";
+                city += "città like '" + buf + "'";
+            }
 
-        pane.setPadding(new Insets(25, 25, 25, 25));
+            if (dateCheck.isSelected() && mainDatePicker.getValue() != null) {
 
-        offerNode[0] = ctyField;
-        offerNode[1] = datePicker;
+                if (clause) date += " and ";
+                else where = " where ";
 
-        return pane;
+                Instant instant = mainDatePicker.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant();
+                Timestamp from = Timestamp.from(instant),
+                        to = Timestamp.from(instant.plus(Duration.ofHours(24)));
+                date += "dataInizio between ";
+                date += "str_to_date('";
+                date += new SimpleDateFormat("yyyy-MM-dd HH:mm").format(from);
+                date += "', '%Y-%m-%d %k:%i') and ";
+                date += "str_to_date('";
+                date += new SimpleDateFormat("yyyy-MM-dd HH:mm").format(to);
+                date += "', '%Y-%m-%d %k:%i')";
+            }
+
+            return query + where + city + date;
+        }
     }
 
-    private Node travelAttachment() {
-        /** @return Node; return a node to attach at the main pane **/
+    private class OvernightOptions extends OfferOptions {
 
-        offerNode = new Node[6];
-        Label departure = new Label("Departure");
-        TextField depField = new MaterialTextField();
-        depField.setPromptText("Insert departure station");
+        @Override
+        protected void fields() {
 
-        DatePicker depTimePicker = new DatePicker(LocalDate.now());
+            super.fields();
+            cityField.setPromptText("Inserisci città");
+            add(new Label("Città"), 0, 0);
+        }
 
-        Label arrival = new Label("Arrival");
-        TextField arrField = new MaterialTextField();
-        arrField.setPromptText("Insert arrival station");
+        @Override
+        String getQuery() {
+            String query = "from PernottamentoEntity", where = "", city = "", date = "";
 
-        DatePicker arrTimePicker = new DatePicker(LocalDate.now());
+            boolean clause = false;
+            String buf;
 
-        Label vehicle = new Label("Vehicol");
-        MaterialSpinner vehSpinner = new MaterialSpinner(this, FXCollections.observableArrayList("Aereo", "Treno", "Bus"));
+            if ((buf = cityField.getText()) != null && !"".equals(buf)) {
+                clause = true;
+                where = " where ";
+                city += "città like '" + buf + "'";
+            }
 
-        Label classLbl = new Label("Class");
-        MaterialSpinner clsSpinner = new MaterialSpinner(this, FXCollections.observableArrayList("First", "Second"));
+            if (dateCheck.isSelected() && mainDatePicker.getValue() != null) {
 
-        GridPane pane = new GridPane();
-        pane.setStyle("-fx-background-color: white");
-        pane.setHgap(25);
-        pane.setVgap(8);
+                if (clause) date += " and ";
+                else where = " where ";
 
-        pane.add(departure, 0, 1);
-        pane.add(arrival, 0, 2);
-        pane.add(vehicle, 0, 3);
-        pane.add(classLbl, 0, 4);
+                Instant instant = mainDatePicker.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant();
+                Timestamp from = Timestamp.from(instant),
+                        to = Timestamp.from(instant.plus(Duration.ofHours(24)));
+                date += "dataInizio between ";
+                date += "str_to_date('";
+                date += new SimpleDateFormat("yyyy-MM-dd HH:mm").format(from);
+                date += "', '%Y-%m-%d %k:%i') and ";
+                date += "str_to_date('";
+                date += new SimpleDateFormat("yyyy-MM-dd HH:mm").format(to);
+                date += "', '%Y-%m-%d %k:%i')";
+            }
 
-        pane.add(depField, 1, 1);
-        pane.add(arrField, 1, 2);
-        pane.add(vehSpinner, 1, 3);
-        pane.add(clsSpinner, 1, 4);
-
-        pane.add(depTimePicker, 3, 1);
-        pane.add(arrTimePicker, 3, 2);
-
-        pane.setPadding(new Insets(25, 25, 25, 25));
-
-        offerNode[0] = depField;
-        offerNode[1] = arrField;
-        offerNode[2] = vehSpinner;
-        offerNode[3] = clsSpinner;
-        offerNode[4] = depTimePicker;
-        offerNode[5] = arrTimePicker;
-
-        return pane;
+            return query + where + city + date;
+        }
     }
 
-    private Node packetAttachment() {
-        /** @return Node; return a node to attach at the main pane **/
+    private class EmptyOptions extends OfferOptions {
 
-        return null;
-    }
-
-    @Override
-    public void harvest() {
-
-        listView = SearchProductController.handle(spinner.getValue(), offerNode, this);
+        @Override protected void generate() { }
+        @Override String getQuery() { return null; }
     }
 }
